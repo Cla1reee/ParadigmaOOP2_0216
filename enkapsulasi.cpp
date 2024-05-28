61a540d146224244fcbd305364b690b8bf169d6a
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2024-05-28 15:51:11 +0700

    membuat main program

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -12,4 +12,17 @@ using namespace std;
         }
  };
 
- 
+ int main() {
+    remotLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu ");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+    cout << lampurRumah.getsaklarNo(0) << endl;
+    cout << lampurRumah.getsaklarNo(1) << endl;
+    cout << lampurRumah.getsaklarNo(2) << endl;
+    cout << lampurRumah.getsaklarNo(3) << endl;
+
+ }
