14568be7694012998406b9644673f48c5c43db3e
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2024-05-28 14:58:10 +0700

    membuat class budi

--- solveMultiWaris.cpp
+++ solveMultiWaris.cpp
@@ -26,3 +26,14 @@ class pelajar : public orang {
                 cout << "pelajar dibuat\n" << endl;
             }
 };
+
+class budi : public pekerja, public pelajar {
+    public:
+
+        budi(int pUmur) :
+            pekerja(pUmur),
+            pelajar(pUmur) {
+                cout << "Budi dibuat\n" << endl;
+            }
+};
+
